b5f9d33b0445ab602d104d00f70091bf968ff485
Vatsal Koradia <koradiavatsal@gmail.com>
2015-09-03 08:53:32 +0530

    Use 4.9 for kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,7 +45,11 @@ BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x01000000
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8916
-TARGET_KERNEL_CONFIG := osprey_defconfig
+TARGET_KERNEL_CONFIG := msm8916-squid_defconfig
+
+# Custom Kernel Toolchain
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 
